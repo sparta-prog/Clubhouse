a7b965af7f7f5ca8f57ecaf5608a606e79187a15
sparta-prog <debarun285@gmail.com>
2023-08-29 22:11:05 +0530

    OTP component added

--- src/components/shared/Steps/StepOtp/StepOtp.jsx
+++ src/components/shared/Steps/StepOtp/StepOtp.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Card from '../../Card/Card';
+import TextInput from '../../TextInput/TextInput';
+import Button from '../../Button/Button';
+import styles from './StepOtp.module.css';
 
 const StepOtp = () => {
-  return (
-    <div>StepOtp</div>
-  )
-}
+    const [otp, setOtp] = useState('');
+    
+    return (
+        <>
+            <div className={styles.cardWrapper}>
+                <Card
+                    title="Enter the code we just texted you"
+                    icon="lock-emoji"
+                >
+                    <TextInput
+                        value={otp}
+                        onChange={(e) => setOtp(e.target.value)}
+                    />
+                    <div className={styles.actionButtonWrap}>
+                        <Button text="Next" />
+                    </div>
+                    <p className={styles.bottomParagraph}>
+                        By entering your number, you’re agreeing to our Terms of
+                        Service and Privacy Policy. Thanks!
+                    </p>
+                </Card>
+            </div>
+        </>
+    );
+};
 
-export default StepOtp
+export default StepOtp;
