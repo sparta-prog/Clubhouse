faf4f20617aba94bada2478181dd64647ddd9e94
sparta-prog <debarun285@gmail.com>
2023-09-09 11:43:21 +0530

    added logout functionality

--- backend/services/token-service.js
+++ backend/services/token-service.js
@@ -47,6 +47,10 @@ class TokenService {
             { token: refreshToken }
         );
     }
+
+    async removeToken(refreshToken) {
+        return await refreshModel.deleteOne({ token: refreshToken });
+    }
 }
 
 module.exports = new TokenService();
